da22d9b50c76f1e29a5c3c1ce85733537af065a2
amir parvaneh <amirparvaneh138@gmail.com>
2021-10-12 11:14:03 +0330

    starting the project

---
+++ src/test/java/com/esg/armenia/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.armenia.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
